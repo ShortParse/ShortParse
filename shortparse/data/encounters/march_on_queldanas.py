8e852fc817e2b16381ff2200ec4c2c5e611b6b21
ShortyWoW <chadricksoup@gmail.com>
2026-05-08 12:15:57 -0400

    Midnight Falls - avoidable damage added to march_on_queldanas.py

--- shortparse/data/encounters/march_on_queldanas.py
+++ shortparse/data/encounters/march_on_queldanas.py
@@ -1,11 +1,62 @@
+# Severity Levels:
+#     "Critical": "critical_count",
+#     "Major": "major_count",
+#     "Warning": "warning_count",
+#     "Info": "info_count",
+
 AVOIDABLE_DAMAGE_BY_ENCOUNTER_ID = {
     # Belo'ren
 
     # Midnight Falls
     3183: {
+        1282008: {
+            "name": "Abyssal Pool",
+            "severity": "Warning",
+        },
+        1249796: {
+            "name": "Shattered Sky",
+            "severity": "Warning",
+        },
+        1263970: {
+            "name": "Heaven's Lance",
+            "severity": "Warning",
+        },
+        1251649: {
+            "name": "Disintegration",
+            "severity": "Minor",
+        },
         1254076: {
             "name": "Heaven's Glaives",
             "severity": "Critical",
         },
+        1279420: {
+            "name": "Dark Quasar",
+            "severity": "Critical",
+        },
+        1254646: {
+            "name": "The Darkwell",
+            "severity": "Critical",
+        },
+        1260264: {
+            "name": "Total Eclipse",
+            "severity": "Critical",
+        },
+        1282034: {
+            "name": "Into the darkwell",
+            "severity": "Critical",
+        },
+        1250898: {
+            "name": "Dark Archangel",
+            "severity": "Critical",
+        },
+        1266388: {
+            "name": "Dark Constellation",
+            "severity": "Critical",
+        },
+        1266897: {
+            "name": "Light Siphon",
+            "severity": "Critical",
+        },
     },
+
 }
--- shortparse/data/encounters/the_voidspire.py
+++ shortparse/data/encounters/the_voidspire.py
@@ -1,3 +1,9 @@
+# Severity Levels:
+#     "Critical": "critical_count",
+#     "Major": "major_count",
+#     "Warning": "warning_count",
+#     "Info": "info_count",
+
 AVOIDABLE_DAMAGE_BY_ENCOUNTER_ID = {
     3183: {  # Midnight Falls
         1254076: {
