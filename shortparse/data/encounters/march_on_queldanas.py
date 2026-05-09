0179bbb1bec38e75ff1c15e7057827072df58965
ShortwireWoW <Chadricksoup@gmail.com>
2026-05-08 21:24:04 -0400

    Removed some unneeded spells.

--- shortparse/data/encounters/march_on_queldanas.py
+++ shortparse/data/encounters/march_on_queldanas.py
@@ -9,46 +9,38 @@ AVOIDABLE_DAMAGE_BY_ENCOUNTER_ID = {
 
     # Midnight Falls
     3183: {
-
-        1282004: {
-            "name": "Abyssal Pool",
-            "severity": "Warning",
-        },
-
-        1249797: {
-            "name": "Shattered Sky",
-            "severity": "Warning",
-        },
-
         1251649: {
             "name": "Disintegration",
             "severity": "Warning",
         },
-
         1254076: {
             "name": "Heaven's Glaives",
             "severity": "Critical",
         },
-
-        1253878: {
-            "name": "Heaven's Lance",
-            "severity": "Warning",
-        },
-
-        1262055: {
-            "name": "Eclipsed",
+        1279420: {
+            "name": "Dark Quasar",
             "severity": "Critical",
         },
-
-        1282458: {
-            "name": "Radiance",
+        1254646: {
+            "name": "The Darkwell",
             "severity": "Critical",
         },
-
-        1254257: {
-            "name": "Tears of L'ura",
+        1282034: {
+            "name": "Into the darkwell",
             "severity": "Critical",
         },
-    }
+        1250898: {
+            "name": "Dark Archangel",
+            "severity": "Critical",
+        },
+        1266388: {
+            "name": "Dark Constellation",
+            "severity": "Critical",
+        },
+        1266897: {
+            "name": "Light Siphon",
+            "severity": "Critical",
+        },
+    },
 
 }
