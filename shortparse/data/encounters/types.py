b10227ccab8435c1a8d1b5c6a124257f1aca1484
ShortwireWoW <Chadricksoup@gmail.com>
2026-05-16 20:25:28 -0400

    Lost some content from the mechanic_builder.md

--- shortparse/data/encounters/types.py
+++ shortparse/data/encounters/types.py
@@ -29,6 +29,7 @@ MechanicCategory: TypeAlias = Literal[
     "debuff_damage",
 ]
 
+
 MechanicFailureType: TypeAlias = Literal[
     "avoidable_damage",
     "missed_interrupt",
@@ -41,6 +42,44 @@ MechanicFailureType: TypeAlias = Literal[
     "stack_failure",
     "boss_range",
 ]
+
+
+CATEGORY_GROUND_EFFECT: MechanicCategory = "ground_effect"
+CATEGORY_SWIRL: MechanicCategory = "swirl"
+CATEGORY_TRAVELING_PROJECTILE: MechanicCategory = "traveling_projectile"
+CATEGORY_BEAM: MechanicCategory = "beam"
+CATEGORY_FRONTAL: MechanicCategory = "frontal"
+CATEGORY_REAR_CONE: MechanicCategory = "rear_cone"
+CATEGORY_FORCED_MOVEMENT: MechanicCategory = "forced_movement"
+CATEGORY_INTERRUPT: MechanicCategory = "interrupt"
+CATEGORY_MINIMUM_SOAK: MechanicCategory = "minimum_soak"
+CATEGORY_SOAK_PARTICIPATION: MechanicCategory = "soak_participation"
+CATEGORY_BAD_SOAK: MechanicCategory = "bad_soak"
+CATEGORY_DISPEL: MechanicCategory = "dispel"
+CATEGORY_SPREAD: MechanicCategory = "spread"
+CATEGORY_STACK: MechanicCategory = "stack"
+CATEGORY_BOSS_THREAT: MechanicCategory = "boss_threat"
+CATEGORY_BOSS_RANGE: MechanicCategory = "boss_range"
+CATEGORY_TANK_BUSTER: MechanicCategory = "tank_buster"
+CATEGORY_TANK_POSITIONING: MechanicCategory = "tank_positioning"
+CATEGORY_ADD_MANAGEMENT: MechanicCategory = "add_management"
+CATEGORY_ADD_PRIORITY: MechanicCategory = "add_priority"
+CATEGORY_CORPSE_EXPLOSION: MechanicCategory = "corpse_explosion"
+CATEGORY_BAIT: MechanicCategory = "bait"
+CATEGORY_LANE_MOVEMENT: MechanicCategory = "lane_movement"
+CATEGORY_DEBUFF_DAMAGE: MechanicCategory = "debuff_damage"
+
+
+FAILURE_AVOIDABLE_DAMAGE: MechanicFailureType = "avoidable_damage"
+FAILURE_MISSED_INTERRUPT: MechanicFailureType = "missed_interrupt"
+FAILURE_MINIMUM_SOAK: MechanicFailureType = "minimum_soak"
+FAILURE_ZERO_PARTICIPATION: MechanicFailureType = "zero_participation"
+FAILURE_BAD_SOAK: MechanicFailureType = "bad_soak"
+FAILURE_MISSED_DISPEL: MechanicFailureType = "missed_dispel"
+FAILURE_BAD_DISPEL: MechanicFailureType = "bad_dispel"
+FAILURE_SPREAD_FAILURE: MechanicFailureType = "spread_failure"
+FAILURE_STACK_FAILURE: MechanicFailureType = "stack_failure"
+FAILURE_BOSS_RANGE: MechanicFailureType = "boss_range"
 
 class Mechanic(TypedDict, total=False):
     name: str
